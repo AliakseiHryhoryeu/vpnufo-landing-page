89c6040b469a8df1ff72e642761d44e441bfab1a
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-07-03 17:30:24 +0300

    add responsive & update preview

--- README.md
+++ README.md
@@ -26,4 +26,8 @@ https://www.figma.com/file/Kis6CkWtqdEGzAx8cLiiid/VPN-ufo?node-id=0%3A1
 
 ## Server
 
-![vpnufo-preview](preview.jpg)
+![Landing-page-preview](./preview-images/preview-1.jpg)
+![Sign-up-preview](./preview-images/preview-2.jpg)
+![Log-in-preview](./preview-images/preview-3.jpg)
+![Reset-Password-preview](./preview-images/preview-4.jpg)
+![Page-404-preview](./preview-images/preview-5.jpg)
--- client/src/app/components/Footer/index.tsx
+++ client/src/app/components/Footer/index.tsx
@@ -3,12 +3,15 @@ import React, { FC } from 'react'
 import './Footer.scss'
 
 import footerImg from 'assets/img/footer_ufo.png'
+import { Link } from 'react-router-dom'
 
 export const Footer: FC = () => {
 	return (
 		<footer className='footer'>
 			<div className='footer__wrapper'>
-				<div className='footer__title'>vpnufo</div>
+				<div className='footer__title'>
+					<Link to='/'>vpnufo</Link>
+				</div>
 				<div className='footer__menu'>
 					<div className='footer__menu-group'>
 						<div className='footer__menu-group-item'>What is a VPN?</div>
@@ -23,7 +26,9 @@ export const Footer: FC = () => {
 					<div className='footer__contacts-img'>
 						<img src={footerImg} alt='footer img' />
 					</div>
-					<div className='footer__contacts-title'>Contact US</div>
+					<div className='footer__contacts-title'>
+						<Link to='/support'>Contact US</Link>
+					</div>
 				</div>
 			</div>
 		</footer>
